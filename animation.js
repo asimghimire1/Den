2b32c3bb6d19b0a8e54f83bd324b8a085d8af877
Asim Ghimire <85211041+asimghimire1@users.noreply.github.com>
2025-05-15 18:19:21 +0545

    Add files via upload
    Hero section, Landing page

---
+++ animation.js
@@ -0,0 +1,12 @@
+
+        document.addEventListener('DOMContentLoaded', function() {
+            var typed = new Typed('#typed', {
+                strings: ["Zero<br>to One", "Ideas<br>to Reality", "Startups<br>to Success"],
+                typeSpeed: 90,
+                backSpeed: 60,
+                loop: true,
+                showCursor: true,
+                cursorChar: '|',
+                autoInsertCss: true
+            });
+        });
